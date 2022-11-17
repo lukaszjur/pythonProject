0938b2bacbb3969f8ca892b18c7c72ea00f3c2e5
Student <lukasz.jurkowski@pwc.com>
2022-11-17 17:00:45 +0100

    Added first two exercises

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,4 +4,5 @@ not_divided_by_100 = year%100!=0
 divided_by_400 = year%400==0
 lap_year = divided_by_400  or (divided_by_4 and not_divided_by_100)
 
+
 print ("is the ", year, " a lap year? ", lap_year)
