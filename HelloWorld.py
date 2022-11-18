93be9a483725514c7c64a7b16b46ac1c5b3e4001
Student <lukasz.jurkowski@pwc.com>
2022-11-18 14:42:38 +0100

    Added first two exercises

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,24 @@
-year = int(input("Please enter a year: "))
-divided_by_4 = year%4 == 0
-not_divided_by_100 = year%100!=0
-divided_by_400 = year%400==0
-lap_year = divided_by_400  or (divided_by_4 and not_divided_by_100)
+def is_it_lap_year(year):
+    divided_by_4 = year % 4 == 0
+    not_divided_by_100 = year % 100 != 0
+    divided_by_400 = year % 400 == 0
+    lap_year = divided_by_400 or (divided_by_4 and not_divided_by_100)
+    if (lap_year):
+        print("Year", year, "is a lap year")
+    else:
+        print("Year", year, "is not a lap year")
+    return lap_year
+    # print("is the", year, "a lap year? ", print ("Yes") if lap_year else print("No"))
 
 
-print ("is the ", year, "  a lap year? ", lap_year)
+if __name__ == '__main__':
+    year = int(input("Please enter a year: "))
+    is_it_lap_year(year)
+
+
+def test_lap_year():
+    assert is_it_lap_year(2020) == True
+
+
+def test_not_lap_year():
+    assert is_it_lap_year(2022) == False
